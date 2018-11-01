397b1e085b0d600796f27e410bb78d209c5f704e
Zoey Dao <yendao.zoey@gmail.com>
2018-11-01 19:34:16 +0700

    homework web 3

--- homework3/app.py
+++ homework3/app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/new_bike", methods=['GET','POST'])
-def register():
+def new_bike():
     if request.method == "GET":
         return  render_template("serious_ex1.html")
     elif request.method == "POST":
-        #1. Unpack data (form)
         form = request.form
-        return form
-        
+        print(form)
+        return "Success"           
+
 if __name__ == '__main__':
   app.run(debug=True)
